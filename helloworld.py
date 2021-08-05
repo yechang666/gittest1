f2d7400623617d611f45078da626a2205086871f
yechang666 <603654962@qq.com>
2021-08-05 23:22:02 +0800

    添加减法运行

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,1 @@
-def add(i, j):
-    m = i + j
-    return m
-
-
-if __name__ == '__main__':
-    cc = add(2, 3)
-    print(cc)
+def add(i, j):    m = i + j    return mdef sub(i, j):    m = i-j    return mif __name__ == '__main__':    cc = add(2, 3)    print(cc)    dd = sub(7, 3)    print(dd)
