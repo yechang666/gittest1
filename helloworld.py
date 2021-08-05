c9b7bd5bd78e292e445e0c978073cfc9ab501a13
yechang666 <603654962@qq.com>
2021-08-05 23:25:47 +0800

    修改编码信息

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,15 @@
-def add(i, j):    m = i + j    return mdef sub(i, j):    m = i-j    return mif __name__ == '__main__':    cc = add(2, 3)    print(cc)    dd = sub(7, 3)    print(dd)
+def add(i, j):
+    m = i + j
+    return m
+
+
+def sub(i, j):
+    m = i-j
+    return m
+
+
+if __name__ == '__main__':
+    cc = add(2, 3)
+    print(cc)
+    dd = sub(7, 3)
+    print(dd)
